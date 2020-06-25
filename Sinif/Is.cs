0992c4050be1c2854b0a27de13799ba9c353244a
emirxmertoglu <emirxmertoglu@outlook.com>
2020-06-25 05:59:22 +0300

    Added necessary classes for Class project

---
+++ Sinif/Is.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinif
+{
+    public class Is
+    {
+        public string Baslik { get; set; }
+        public enum Zamanlama
+        {
+            PartTime,
+            FullTime
+        }
+        public DateTime YayinTarihi { get; set; }
+        public int Kontenjan { get; set; }
+        public int Maas { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
--- Sinif/Kisi.cs
+++ Sinif/Kisi.cs
@@ -8,6 +8,17 @@ namespace Sinif
 {
     public abstract class Kisi
     {
-
+        public string AdSoyad { get; set; }
+        public string Firma { get; set; }
+        public string Unvan { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public string Bolge { get; set; }
+        public string Sifre { get; set; }
+        public enum Cinsiyet
+        {
+            Erkek,
+            Kadın
+        }
     }
 }
